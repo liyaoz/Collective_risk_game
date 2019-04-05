fdfc7dfb59c71bf6241f12fd8c0da162e60eb73e
Liyao Zhu <stevens.zhu@gmail.com>
2019-04-05 15:52:36 +1100

    game.py and agent.py structure build2

--- agent.py
+++ agent.py
@@ -7,10 +7,15 @@ class Agent():
 
     def __init__(self):
 
+        'add variables to construct agent instance and define instance variables'
+
         self.undefined = None
+
+
 
 
     def chooseAction(selfs):
 
+        'implement your strategy here'
+
         return 0
-        
